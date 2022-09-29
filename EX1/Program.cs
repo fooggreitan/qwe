dbdaaebf8d24ca7076327318b818610ced30bb17
LEN4 <fooggreitan@gmail.com>
2022-09-29 21:46:54 +0300

    10

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("12 Hello, World!");
+﻿Console.Write("Имя: ");
+string user = Console.ReadLine();
+
+if(user.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(user);
+}
